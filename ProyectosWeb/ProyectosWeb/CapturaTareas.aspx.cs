262de5eadad67618ff5cf860c80fad61c43439a7
KiehlisaDog <juanl.borregom@gmail.com>
2014-07-23 10:29:37 -0500

    Actualizacion230714

--- ProyectosWeb/ProyectosWeb/CapturaTareas.aspx.cs
+++ ProyectosWeb/ProyectosWeb/CapturaTareas.aspx.cs
@@ -15,5 +15,42 @@ namespace ProyectosWeb
 
 
         }
+         
+        protected void ButtonUpload_Click(object sender, EventArgs e)
+        {
+            Boolean fileOK = false;
+            String path = Server.MapPath("~/UploadedExcels/");
+            if (FileUploadTareas.HasFile)
+            {
+                String fileExtension =
+                    System.IO.Path.GetExtension(FileUploadTareas.FileName).ToLower();
+                String[] allowedExtensions = { ".xls", ".xlsx"};
+                for (int i = 0; i < allowedExtensions.Length; i++)
+                {
+                    if (fileExtension == allowedExtensions[i])
+                    {
+                        fileOK = true;
+                    }
+                }
+            }
+
+            if (fileOK)
+            {
+                try
+                {
+                    FileUploadTareas.PostedFile.SaveAs(path
+                        + FileUploadTareas.FileName);
+                    LabelSubir.Text = "Tareas cargadas con exito";
+                }
+                catch (Exception ex)
+                {
+                    LabelSubir.Text = "Error en carga de tareas";
+                }
+            }
+            else
+            {
+                LabelSubir.Text = "No se aceptan archivos de este tipo";
+            }
+        }
     }
 }
